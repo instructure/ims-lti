f5b9aa7a34a1a93d2ea1c64ec9915d2514e3501a
Nathan Mills <nathanm@instructure.com>
2014-11-20 10:51:30 -0500

    fix oauth headers, and miss spellings

--- lib/ims/lti/models/base_url_choice.rb
+++ lib/ims/lti/models/base_url_choice.rb
@@ -4,7 +4,7 @@ module IMS::LTI::Models
     add_attribute :selector, relation:'IMS::LTI::Models::BaseUrlSelector'
 
     def default_message_url
-      if selector && selector.applies_to && selector.applies_to.include?('MessageHandler')
+      if selector.nil? || (selector.applies_to && selector.applies_to.include?('MessageHandler'))
         secure_base_url || default_base_url
       else
         ''
--- lib/ims/lti/models/service_owner.rb
+++ lib/ims/lti/models/service_owner.rb
@@ -4,5 +4,21 @@ module IMS::LTI::Models
     add_attribute :timestamp, json_converter:'IMS::LTI::Converters::TimeJSONConverter'
     add_attribute :service_owner_name, relation: 'IMS::LTI::Models::LocalizedName'
 
+    def create_service_owner_name(name, key = 'service_owner.name')
+      @service_owner_name = LocalizedName.new(name, key)
+    end
+
+    def create_description(name, key = 'service_owner.description')
+      @description = LocalizedText.new(name, key)
+    end
+
+    def default_name
+      service_owner_name && service_owner_name.default_value
+    end
+
+    def default_description
+      description && description.default_value
+    end
+
   end
 end
