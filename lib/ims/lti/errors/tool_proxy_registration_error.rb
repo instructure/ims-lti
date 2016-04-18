1c0fdf6b1bd074f8c12c769e3e4d1206207e933a
Tyler Pickett <t.pickett66@gmail.com>
2016-04-18 15:37:30 -0600

    Improve the message for ToolProxyRegistrationErrors

--- lib/ims/lti/errors/tool_proxy_registration_error.rb
+++ lib/ims/lti/errors/tool_proxy_registration_error.rb
@@ -5,7 +5,11 @@ module IMS::LTI::Errors
     def initialize(response_status, response_body = nil)
       @response_status = response_status
       @response_body = response_body
+      super <<-EOF
+Unexpected response for tool proxy registration.
+HTTP Response Status: #{response_status}
+HTTP Response Body: #{response_body}
+EOF
     end
-
   end
 end
