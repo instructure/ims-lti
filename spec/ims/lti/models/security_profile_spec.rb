ce6c7cb8c328cc632567c54d45eab01dc5ca32da
Nathan Mills <nathanm@instructure.com>
2017-03-27 11:27:29 -0600

    add specs for security_profile

--- spec/ims/lti/models/security_profile_spec.rb
+++ spec/ims/lti/models/security_profile_spec.rb
@@ -3,5 +3,19 @@ require 'spec_helper'
 module IMS::LTI::Models
   describe SecurityProfile do
 
+    it 'has a security_profile_name attribute' do
+      subject.security_profile_name = 'test'
+      expect(subject.security_profile_name).to eq 'test'
+    end
+
+    it 'has a digest_algorithm attribute' do
+      subject.digest_algorithm = 'test'
+      expect(subject.digest_algorithm).to eq 'test'
+    end
+
+    it 'pluralizes digest_algoritim' do
+      expect(subject.digest_algorithms).to eq []
+    end
+
   end
 end
