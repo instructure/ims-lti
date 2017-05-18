e4e01e6cc9e2cf891660f3e1f238b26fdf0faed8
Nathan Mills <rivernate@users.noreply.github.com>
2017-05-18 16:16:43 -0600

    add support for jwt launches (#116)
    * add support for jwt launches
    
    * make specs clearer

--- spec/ims/lti/models/messages/message_spec.rb
+++ spec/ims/lti/models/messages/message_spec.rb
@@ -2,50 +2,53 @@ require 'spec_helper'
 
 module IMS::LTI::Models::Messages
   describe Message do
+
+    subject(:message) {Message.new}
+
     describe 'parameters' do
       it "returns params for the message" do
-        subject.lti_message_type = 'message-type'
+        message.lti_message_type = 'message-type'
 
-        params = subject.post_params
+        params = message.post_params
         expect(params['lti_message_type']).to eq('message-type')
       end
 
       it 'returns custom params for the message' do
-        subject.custom_name = 'my_custom_name'
+        message.custom_name = 'my_custom_name'
 
-        params = subject.post_params
+        params = message.post_params
         expect(params['custom_name']).to eq('my_custom_name')
       end
 
       it 'returns ext params for the message' do
-        subject.ext_name = 'my_ext_name'
+        message.ext_name = 'my_ext_name'
 
-        params = subject.post_params
+        params = message.post_params
         expect(params['ext_name']).to eq('my_ext_name')
       end
 
       it 'sets a ext param' do
-        subject.ext_name = 'my_ext_name'
-        expect(subject.ext_name).to eq 'my_ext_name'
+        message.ext_name = 'my_ext_name'
+        expect(message.ext_name).to eq 'my_ext_name'
       end
 
       it 'sets an custom param' do
-        subject.custom_name = 'my_custom_name'
-        expect(subject.custom_name).to eq 'my_custom_name'
+        message.custom_name = 'my_custom_name'
+        expect(message.custom_name).to eq 'my_custom_name'
       end
 
       it 'returns all the custom params' do
-        subject.custom_name = 'my_custom_name'
-        subject.custom_number = '3'
-        params = subject.post_params
+        message.custom_name = 'my_custom_name'
+        message.custom_number = '3'
+        params = message.post_params
         expect(params['custom_name']).to eq 'my_custom_name'
         expect(params['custom_number']).to eq '3'
       end
 
       it 'returns all the ext params' do
-        subject.ext_name = 'my_ext_name'
-        subject.ext_number = '42'
-        params = subject.post_params
+        message.ext_name = 'my_ext_name'
+        message.ext_number = '42'
+        params = message.post_params
         expect(params['ext_name']).to eq 'my_ext_name'
         expect(params['ext_number']).to eq '42'
       end
@@ -57,7 +60,7 @@ module IMS::LTI::Models::Messages
 
       it 'returns unknown params' do
         message = described_class.new(foo: 'bar')
-        expect(message.unknown_params).to eq({'foo' => 'bar'})
+        expect(message.unknown_params).to eq({ 'foo' => 'bar' })
       end
 
       describe 'parameters' do
@@ -65,15 +68,15 @@ module IMS::LTI::Models::Messages
           add_deprecated_params :foo
         end
 
-        subject {CustomMesage.new(lti_version: '1', foo: 'bar')}
+        subject(:message) {CustomMesage.new(lti_version: '1', foo: 'bar')}
 
 
         it 'returns all the supported params' do
-          expect(subject.parameters).to eq({"lti_version"=>"1", "foo"=>"bar"})
+          expect(message.parameters).to eq({ "lti_version" => "1", "foo" => "bar" })
         end
 
         it 'returns the required params' do
-          expect(subject.required_params).to eq({"lti_version"=>"1"})
+          expect(message.required_params).to eq({ "lti_version" => "1" })
         end
 
       end
@@ -106,15 +109,15 @@ module IMS::LTI::Models::Messages
     describe '#add_custom_params' do
 
       it 'prepends "custom_" to the param' do
-        subject.add_custom_params('foo' => 'bar')
-        params = subject.instance_variable_get('@custom_params')
+        message.add_custom_params('foo' => 'bar')
+        params = message.instance_variable_get('@custom_params')
         expect(params.keys.size).to eq 1
         expect(params.keys.first).to eq 'custom_foo'
       end
 
       it 'handles symbols' do
-        subject.add_custom_params(foo: 'bar')
-        params = subject.instance_variable_get('@custom_params')
+        message.add_custom_params(foo: 'bar')
+        params = message.instance_variable_get('@custom_params')
         expect(params.keys.first).to eq 'custom_foo'
       end
 
@@ -122,8 +125,8 @@ module IMS::LTI::Models::Messages
 
     describe '#get_custom_params' do
       it 'removes "custom_" to the parameter' do
-        subject.add_custom_params('custom_foo' => 'bar')
-        params = subject.get_custom_params
+        message.add_custom_params('custom_foo' => 'bar')
+        params = message.get_custom_params
         expect(params.keys.size).to eq 1
         expect(params.keys.first).to eq 'foo'
       end
@@ -132,11 +135,11 @@ module IMS::LTI::Models::Messages
     context 'OAuth' do
       describe "#signed_post_params" do
         it "creates a hash with the oauth signature" do
-          subject.custom_user_id = 'user_id'
-          subject.launch_url = 'http://www.example.com'
-          subject.oauth_consumer_key = 'key'
+          message.custom_user_id = 'user_id'
+          message.launch_url = 'http://www.example.com'
+          message.oauth_consumer_key = 'key'
 
-          params = subject.signed_post_params('secret')
+          params = message.signed_post_params('secret')
 
           expect(params[:oauth_consumer_key]).to eq "key"
           expect(params[:oauth_signature_method]).to eq "HMAC-SHA1"
@@ -152,30 +155,105 @@ module IMS::LTI::Models::Messages
     describe "#generate" do
 
       it 'generates a BasicLTILaunchRequest message' do
-        message = described_class.generate({'lti_message_type' => BasicLTILaunchRequest::MESSAGE_TYPE })
+        message = described_class.generate({ 'lti_message_type' => BasicLTILaunchRequest::MESSAGE_TYPE })
         expect(message).to be_a BasicLTILaunchRequest
       end
 
       it 'generates a RegistrationRequest message' do
-        message = described_class.generate({'lti_message_type' => RegistrationRequest::MESSAGE_TYPE })
+        message = described_class.generate({ 'lti_message_type' => RegistrationRequest::MESSAGE_TYPE })
         expect(message).to be_a RegistrationRequest
       end
 
       it 'generates a ContentItemSelectionRequest message' do
-        message = described_class.generate({'lti_message_type' => ContentItemSelectionRequest::MESSAGE_TYPE })
+        message = described_class.generate({ 'lti_message_type' => ContentItemSelectionRequest::MESSAGE_TYPE })
         expect(message).to be_a ContentItemSelectionRequest
       end
 
       it 'generates a ContentItemSelection message' do
-        message = described_class.generate({'lti_message_type' => ContentItemSelection::MESSAGE_TYPE })
+        message = described_class.generate({ 'lti_message_type' => ContentItemSelection::MESSAGE_TYPE })
         expect(message).to be_a ContentItemSelection
       end
 
       it 'generates a ToolProxyReregistration message' do
-        message = described_class.generate({'lti_message_type' => ToolProxyReregistrationRequest::MESSAGE_TYPE})
+        message = described_class.generate({ 'lti_message_type' => ToolProxyReregistrationRequest::MESSAGE_TYPE })
         expect(message).to be_a ToolProxyReregistrationRequest
       end
 
+
+    end
+
+    describe "#to_jwt" do
+      let(:private_key) {'secret'}
+      let(:launch_url) {'http://www.example.com'}
+      let(:originating_domain) {'instructure.com'}
+      let(:consumer_key) {'key'}
+
+      subject(:message) {Message.new(consumer_key: consumer_key)}
+
+      it "sets the 'iss' to the originating domain" do
+        jwt = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+        json = JSON::JWT.decode(jwt, private_key)
+        expect(json[:iss]).to eq originating_domain
+      end
+
+      it "sets the 'sub' to the consumer_key" do
+        jwt = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+        json = JSON::JWT.decode(jwt, private_key)
+        expect(json[:sub]).to eq consumer_key
+      end
+
+      it "sets the 'aud' to the launch_url" do
+        message.launch_url = launch_url
+        jwt = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+        json = JSON::JWT.decode(jwt, private_key)
+        expect(json[:aud]).to eq launch_url
+      end
+
+      it "sets the iat to the current time" do
+        Timecop.freeze do
+          jwt = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+          json = JSON::JWT.decode(jwt, private_key)
+          expect(json[:iat]).to eq Time.now.to_i
+        end
+      end
+
+      it "sets the exp to 5 minutes in the future" do
+        Timecop.freeze do
+          jwt = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+          json = JSON::JWT.decode(jwt, private_key)
+          expect(json[:exp]).to eq (Time.now + 60 * 5).to_i
+        end
+      end
+
+      it "sets a 'jti'" do
+        jwt = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+        json = JSON::JWT.decode(jwt, private_key)
+        expect(json[:jti]).to_not be_nil
+      end
+
+      it "creates a new jti each time" do
+        jwt1 = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+        jwt2 = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+        json1 = JSON::JWT.decode(jwt1, private_key)
+        json2 = JSON::JWT.decode(jwt2, private_key)
+        expect(json1[:jti]).to_not eq json2[:jti]
+      end
+
+      context "ims claims" do
+        let(:user_id) {"user_id"}
+        it 'sets ims claims' do
+          message.custom_user_id = user_id
+          message.ext_user_role = %w(student teacher)
+          jwt = message.to_jwt(private_key: private_key, originating_domain: originating_domain)
+          json = JSON::JWT.decode(jwt, private_key)[:"org.imsglobal.lti.message"]
+          expect(json).to eq (
+                               {
+                                 "custom_user_id" => message.custom_user_id,
+                                 "ext_user_role" => message.ext_user_role
+                               }
+                             )
+        end
+      end
 
     end
 
