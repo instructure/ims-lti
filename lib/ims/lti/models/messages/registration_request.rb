c14bd90faa2f23341f35e4551fca171539b1bed2
Nathan Mills <nathanm@instructure.com>
2017-07-12 18:11:02 -0600

    add additonal params to the registration message

--- lib/ims/lti/models/messages/registration_request.rb
+++ lib/ims/lti/models/messages/registration_request.rb
@@ -1,7 +1,8 @@
 module IMS::LTI::Models::Messages
   class RegistrationRequest < RequestMessage
 
-    add_required_params :reg_key, :reg_password, :tc_profile_url, :launch_presentation_return_url
+    add_required_params :reg_key, :reg_password, :tc_profile_url, :launch_presentation_return_url, :tool_proxy_guid,
+                        :tool_proxy_url
 
 
     MESSAGE_TYPE = 'ToolProxyRegistrationRequest'
@@ -12,7 +13,7 @@ module IMS::LTI::Models::Messages
     end
 
     def generate_key_and_password
-      self.reg_key, self.reg_password = 2.times.map { SecureRandom.uuid }
+      self.reg_key, self.reg_password = 2.times.map {SecureRandom.uuid}
     end
 
   end
