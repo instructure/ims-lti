f825b5520a34ba5af8c0aa5d4b366034a38e360d
wdransfield <wdransfield@instructure.com>
2017-08-14 09:30:10 -0600

    Remove debug logging and enhance AthenticationError

--- lib/ims/lti/errors/authentication_failed_error.rb
+++ lib/ims/lti/errors/authentication_failed_error.rb
@@ -1,10 +1,12 @@
 module IMS::LTI::Errors
   class AuthenticationFailedError < StandardError
 
-    attr_reader :response
+    attr_reader :response, :assertion, :grant_type
 
-    def initialize(response: nil)
+    def initialize(response: nil, assertion: nil)
       @response = response
+      @assertion = assertion
+      @grant_type
     end
 
   end
