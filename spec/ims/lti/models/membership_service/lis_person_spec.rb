2054243f8fe949d009c6a5ffd8a1906363471862
Marc Phillips <mphillips@instructure.com>
2019-09-24 12:40:14 -0600

    Make sis_id optional
    Add test to verify behavior.

--- spec/ims/lti/models/membership_service/lis_person_spec.rb
+++ spec/ims/lti/models/membership_service/lis_person_spec.rb
@@ -2,13 +2,8 @@ require 'spec_helper'
 
 module IMS::LTI::Models::MembershipService
   describe LISPerson do
-    it 'includes IMS::LTI::Models::Serializable' do
-      includes = LISPerson.ancestors.select { |o| o.class == Module }
-      expect(includes.include?(IMS::LTI::Models::Serializable)).to eq true
-    end
-
-    let(:lis_person) do
-      lis_person = LISPerson.new({
+    let(:lis_person_params) do
+      {
         id: 1,
         name: 'Test',
         family_name: 'Case',
@@ -18,7 +13,15 @@ module IMS::LTI::Models::MembershipService
         result_sourced_id: 1,
         sourced_id: 1,
         user_id: 1
-      })
+      }
+    end
+    let(:lis_person) do
+      lis_person = LISPerson.new(lis_person_params)
+    end
+
+    it 'includes IMS::LTI::Models::Serializable' do
+      includes = LISPerson.ancestors.select { |o| o.class == Module }
+      expect(includes.include?(IMS::LTI::Models::Serializable)).to eq true
     end
 
     describe '#initialize' do
@@ -47,6 +50,27 @@ module IMS::LTI::Models::MembershipService
         expect(hash.fetch(:resultSourcedId)).to eq 1
         expect(hash.fetch(:sourcedId)).to eq 1
         expect(hash.fetch(:userId)).to eq 1
+        expect(hash).not_to have_key(:sis_id)
+      end
+
+      context 'with sis_id' do
+        let(:lis_person_params) do
+          super().merge(sis_id: 'test')
+        end
+
+        it 'serializes' do
+          hash = lis_person.as_json
+          expect(hash.fetch(:@id)).to eq 1
+          expect(hash.fetch(:name)).to eq 'Test'
+          expect(hash.fetch(:img)).to eq 'url'
+          expect(hash.fetch(:email)).to eq 'email'
+          expect(hash.fetch(:familyName)).to eq 'Case'
+          expect(hash.fetch(:givenName)).to eq 'Test'
+          expect(hash.fetch(:resultSourcedId)).to eq 1
+          expect(hash.fetch(:sourcedId)).to eq 1
+          expect(hash.fetch(:userId)).to eq 1
+          expect(hash.fetch(:sisId)).to eq 'test'
+        end
       end
     end
   end
